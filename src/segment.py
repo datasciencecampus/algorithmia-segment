b68780fde4386b689886dda26bdd693d9e89f2be
phil8192 <phil@parasec.net>
2018-09-30 10:04:26 +0000

    use dsc chainer-pspnet fork as submodule

--- src/segment.py
+++ src/segment.py
@@ -7,7 +7,7 @@ from chainercv.utils import read_image
 
 # see caveats section in:
 # https://algorithmia.com/developers/algorithm-development/languages/python/
-from .chainer_pspnet.pspnet import PSPNet
+from .dsc_chainer_pspnet.pspnet import PSPNet
 
 from PIL import Image
 from glob import glob
