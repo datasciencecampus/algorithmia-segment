c95566d1b575dadd79efc0af61ad4d74060e4c90
phil8192 <phil@parasec.net>
2018-09-30 16:51:47 +0000

    update run_all.sh docs

--- src/client/run_all.sh
+++ src/client/run_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# demo.
+################################################################################
+# Segmentation demo                                                            #
+# ============================================================================ #
+# ./run_all.sh                                                                 #
+#                                                                              #
+# Phil Stubbings, ONS Data Science Campus.                                     #
+################################################################################
+
 python3 example.py $KEY $SERVER test_images \
   "data://.my/test_images" "data://.algo/nocturne/segment/temp" result
 python3 visualise.py test_images result visualise
